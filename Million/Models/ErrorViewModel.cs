085a43429ad039be75e6109ed90cdaacefc7122c
Doskarayev Nurlan <n.doskaraev@agrostream.net>
2018-11-06 20:35:26 +0600

    Add project files.

---
+++ Million/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Million.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
